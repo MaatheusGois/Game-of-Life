8267505b8f4f0c23af162d1317ca61dd8e3b1a72
Gois <matheusgoislimasilva@gmail.com>
2019-10-31 15:26:09 -0300

    Initial Commit

---
+++ game-of-life-mac/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  game-of-life-mac
+//
+//  Created by Matheus Silva on 31/10/19.
+//  Copyright © 2019 Matheus Gois. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
